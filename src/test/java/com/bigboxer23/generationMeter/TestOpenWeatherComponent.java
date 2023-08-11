15e4af22387b5fd52905bd240e52575040d15741
Matt Jones <bigboxer23@gmail.com>
2023-08-10 23:39:02 -0500

    Add testing to weather component (#75)
    * add cache to the weather service so we don't spam all our API hits
    
    * add testing code

---
+++ src/test/java/com/bigboxer23/generationMeter/TestOpenWeatherComponent.java
@@ -0,0 +1,41 @@
+package com.bigboxer23.generationMeter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.bigboxer23.generationMeter.data.Location;
+import com.bigboxer23.generationMeter.data.WeatherSystemData;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/** */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class TestOpenWeatherComponent {
+
+	@Autowired
+	private OpenWeatherComponent component;
+
+	@Test
+	public void testGetLatLongFromCity() {
+		Location location = component.getLatLongFromCity("golden valley", "mn", 581);
+		assertNotNull(location);
+		assertEquals(location.getLat(), 44.9861176);
+		assertEquals(location.getLon(), -93.3784618);
+		assertFalse(location.isFromCache());
+		location = component.getLatLongFromCity("golden valley", "mn", 581);
+		assertTrue(location.isFromCache());
+	}
+
+	@Test
+	public void testGetSunriseSunsetFromCityStateCountry() {
+		WeatherSystemData data = component.getSunriseSunsetFromCityStateCountry("golden valley", "mn", 581);
+		assertNotNull(data);
+		assertTrue(data.getSunrise() > -1);
+		assertTrue(data.getSunset() > -1);
+		assertFalse(data.isFromCache());
+		data = component.getSunriseSunsetFromCityStateCountry("golden valley", "mn", 581);
+		assertTrue(data.isFromCache());
+	}
+}
